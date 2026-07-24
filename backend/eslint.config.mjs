4838eb28fa60205022e4d803d2903c81730764ca
Marcos Garrido <11638875+marcosgarrido@users.noreply.github.com>
2026-07-24 21:14:39 +0200

    Add ESLint and Prettier to backend

---
+++ backend/eslint.config.mjs
@@ -0,0 +1,54 @@
+import _import from "eslint-plugin-import";
+import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
+import { defineConfig, globalIgnores } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
+  globalIgnores(["dist/**/*"]),
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: { globals: globals.node },
+  },
+  tseslint.configs.recommended,
+  eslintPluginPrettier,
+  {
+    plugins: {
+      import: _import,
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_", caughtErrorsIgnorePattern: "^_" },
+      ],
+      "capitalized-comments": ["warn", "always"],
+      "import/order": [
+        "warn",
+        {
+          groups: [
+            "type",
+            "builtin",
+            "object",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+          ],
+          pathGroups: [
+            {
+              pattern: "@/**",
+              group: "internal",
+              position: "after",
+            },
+          ],
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+          "newlines-between": "always",
+        },
+      ],
+    },
+  },
+]);
